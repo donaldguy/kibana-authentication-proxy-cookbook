afffbae7114ac7fc37b64263a49a184d67b1b55b
Donald Guy <donaldg@crowdtap.com>
2014-08-22 11:15:04 -0400

    Refactor user recipe to use local variables

--- recipes/user.rb
+++ recipes/user.rb
@@ -1,4 +1,6 @@
-user node[:kibana_authentication_proxy][:user] do
+kibana_user = node[:kibana_authentication_proxy][:user]
+
+user kibana_user do
   system true
   home node[:kibana_authentication_proxy][:install_path]
   action :create
@@ -10,8 +12,9 @@ if node[:kibana_authentication_proxy][:use_authbind]
     include_recipe 'authbind'
   end
 
-  authbind_port "kibana user" do
-    port node[:kibana_authentication_proxy][:listen_port]
-    user node[:kibana_authentication_proxy][:user]
+  port_to_grant = node[:kibana_authentication_proxy][:listen_port]
+  authbind_port "#{kibana_user} can bind #{port_to_grant}" do
+    port port_to_grant
+    user kibana_user
   end
 end
